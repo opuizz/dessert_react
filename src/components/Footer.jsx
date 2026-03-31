13188e5ae359d014bdd1ce1ea776744402a55dae
opuizz <0203dlsrud@gmail.com>
2026-03-31 11:34:59 +0900

    컴포넌트 완료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,120 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer id="footerType" className="section">
+      <h2 className="blind">푸터 영역</h2>
+      <div className="footer_inner container">
+        <div className="footer_menu">
+          <div>
+            <h2>투썸플레이스</h2>
+            <ul>
+              <li>
+                <a href="#">웹표준 사이트</a>
+              </li>
+              <li>
+                <a href="#">반응형 사이트</a>
+              </li>
+              <li>
+                <a href="#">패럴럭스 사이트</a>
+              </li>
+              <li>
+                <a href="#">포트폴리오 사이트</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>파리바게뜨</h2>
+            <ul>
+              <li>
+                <a href="#">웹표준 사이트</a>
+              </li>
+              <li>
+                <a href="#">반응형 사이트</a>
+              </li>
+              <li>
+                <a href="#">패럴럭스 사이트</a>
+              </li>
+              <li>
+                <a href="#">포트폴리오 사이트</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>뚜레쥬르</h2>
+            <ul>
+              <li>
+                <a href="#">웹표준 사이트</a>
+              </li>
+              <li>
+                <a href="#">반응형 사이트</a>
+              </li>
+              <li>
+                <a href="#">패럴럭스 사이트</a>
+              </li>
+              <li>
+                <a href="#">포트폴리오 사이트</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>스타벅스</h2>
+            <ul>
+              <li>
+                <a href="#">웹표준 사이트</a>
+              </li>
+              <li>
+                <a href="#">반응형 사이트</a>
+              </li>
+              <li>
+                <a href="#">패럴럭스 사이트</a>
+              </li>
+              <li>
+                <a href="#">포트폴리오 사이트</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>메가커피</h2>
+            <ul>
+              <li>
+                <a href="#">웹표준 사이트</a>
+              </li>
+              <li>
+                <a href="#">반응형 사이트</a>
+              </li>
+              <li>
+                <a href="#">패럴럭스 사이트</a>
+              </li>
+              <li>
+                <a href="#">포트폴리오 사이트</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>컴포즈커피</h2>
+            <ul>
+              <li>
+                <a href="#">웹표준 사이트</a>
+              </li>
+              <li>
+                <a href="#">반응형 사이트</a>
+              </li>
+              <li>
+                <a href="#">패럴럭스 사이트</a>
+              </li>
+              <li>
+                <a href="#">포트폴리오 사이트</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer_right">
+          2021 A TWOSOME PLACE Co. LTD.<br>All Rights Reserved.</br>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
